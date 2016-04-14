be81f85706be63d2fa20470ee583b64983efaa6c
陈欣 <95323362@qq.com>
2016-04-14 10:06:42 +0800

    -

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -13,7 +13,49 @@ Space complexity should be O(n).
 Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
 
 
+update v3.0:
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> v(num + 1);
+        v[0] = 0;
+        for(int i = 1; i < num + 1; i++) {
+            v[i] = v[i >> 1] + i % 2;
+        }
+        return v;
+    }
+};
 
+
+update v2.0:
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> v(num + 1);
+        v[0] = 0;
+        v[1] = 1;
+        v[2] = 1;
+        v[3] = 2;
+        for(int i = 4; i < num + 1; i = i * 2) {
+            for(int j = i; j < i + i/2 && j < num + 1; j++) {
+                v[j] = v[j - i/2];
+            }
+            for(int j = i + i/2; j < i * 2 && j < num + 1; j++) {
+                int temp = j;
+                int cnt = 0;
+                while(temp) {
+                    cnt = cnt + temp % 2;
+                    temp = temp/2;
+                }
+                v[j] = cnt;
+            }
+        }
+        return v;
+    }
+};
+
+
+version v1.0
 class Solution {
 public:
     vector<int> countBits(int num) {
