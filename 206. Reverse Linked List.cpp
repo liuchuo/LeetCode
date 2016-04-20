6bf560e5391b163b8489aaaaaa283652f9b19d59
陈欣 <95323362@qq.com>
2016-04-20 09:05:44 +0800

    -

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -5,6 +5,41 @@ click to show more hints.
 
 Subscribe to see which companies asked this question
 
+
+update v2.0
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* func(ListNode* head) {
+        if(head->next != NULL) {
+            ListNode *tail = func(head->next);
+            tail->next = head;
+            head->next = NULL;
+        }
+        return head;
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) 
+            return head;
+        ListNode *newhead = head;
+        while(newhead->next != NULL) {
+            newhead = newhead->next;
+        }
+        func(head);
+        return newhead;
+    }
+};
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
