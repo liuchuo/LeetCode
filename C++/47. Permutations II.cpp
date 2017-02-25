7112384f33d4fc7d1bb0d6b3e3ff2ad20dad7d17
liuchuo <95323362@qq.com>
2017-02-25 11:33:30 +0800

    -

---
+++ C++/47. Permutations II.cpp
@@ -0,0 +1,25 @@
+47. Permutations II
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+题目大意：返回一个数字集合构成的数字（可能有重复数字）的所有可能的唯一的全排列～
+分析：先对nums排序，然后将所有全排列加入result中～
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        do {
+            result.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return result;
+    }
+};
